3ad98ec0ea74ea896a3c92abbd8d14800ddf0c97
Ronal Juma Vasquez <64800264+rjv576@users.noreply.github.com>
2025-01-11 15:23:53 -0400

    creando templates v1

--- debt_manager/debt_manager/settings.py
+++ debt_manager/debt_manager/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'debt_manager.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,8 +116,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+# Ubicación donde se recopilarán los archivos estáticos
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+# Configuración existente
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+# Para producción, asegúrate de que este parámetro esté configurado correctamente
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Donde se recopilan los archivos estáticos
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- debt_manager/debts/views.py
+++ debt_manager/debts/views.py
@@ -1,5 +1,38 @@
 from django.shortcuts import render
-
+from .models import Debt, PaymentMethod
 # Create your views here.
-def index(request):
-    return render(request, 'debts/index.html')
+def base(request):
+    return render(request, 'debts/base.html')
+
+def debt_list(request):
+    debts = Debt.objects.all()
+    return render(request, 'debts/debt_list.html', {'debts': debts})
+
+def payment_method_list(request):
+    payment_methods = PaymentMethod.objects.all()
+    return render(request, 'debts/payment_method_list.html', {'payment_methods': payment_methods})
+
+def calculate_payments(request, debt_id):
+    debt = Debt.objects.get(id=debt_id)
+    payment_methods = PaymentMethod.objects.filter(debt=debt)
+    
+    # Logica para calcular la distribucion de los pagos y pechas 
+    # Supniendo que solo se tiene un metododo de pago poor deuda
+    if payment_methods.exists():
+        payment_method = payment_methods.first()
+        # Calculamos la cantidad de meses necesarios para pagar la deuda
+        months_to_pay = debt.balance / payment_method.monthly_payment
+        total_paid = payment_method.monthly_payment * months_to_pay
+        total_saved = total_paid - debt.balance
+    else:
+        months_to_pay = 0
+        total_paid = 0
+        total_saved = 0
+        
+        return render(request, 'debts/calculate_payments.html', {
+            'debt': debt,
+            'payment_method': payment_method,
+            'months_to_pay': months_to_pay,
+            'total_paid': total_paid,
+            'total_saved': total_saved
+        })
